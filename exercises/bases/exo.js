35d03186941e29590ad1b3b111a99bb25764eda8
Brice Marchand <brice.marchand91@gmail.com>
2020-02-23 15:14:09 +0100

    add spread operator exercice

--- exercises/bases/exo.js
+++ exercises/bases/exo.js
@@ -1,8 +1,13 @@
-
 function testDestructuring(arrayIndex, objectName, restLength){
   // Vous ne devez rien changer ici, juste utiliser les mêmes noms de variable
   return arrayIndex + objectName + restLength.length;
 }
 
+// spread operator
+function updateUserInformation(userList, userId, newInformation) {
+  return {
+    // renvoyer directement la nouvelle userList avec l'utilisateur mis à jour
+  };
+}
 
-module.exports = {testDestructuring};
+module.exports = {testDestructuring, updateUserInformation};
--- exercises/bases/test.spec.js
+++ exercises/bases/test.spec.js
@@ -1,19 +1,74 @@
-const {testDestructuring} = require('./exo.js');
+const { testDestructuring, updateUserInformation } = require('./exo.js');
 
-describe('Test exercise for bases', function () {
-
-  let instanceToTest;
+describe('Test exercise for bases', function() {
+  const userList = {
+    234: {
+      id: 234,
+      name: 'Paul',
+      city: 'London'
+    },
+    235: {
+      id: 235,
+      name: 'Pierre',
+      city: 'Paris'
+    },
+    236: {
+      id: 236,
+      name: 'Jack',
+      city: 'Lyon'
+    }
+  };
 
   it('Test testDestructuring() 1', () => {
-    expect(testDestructuring([1,2,3], {name: 'plop'}, 1, 2, 'plop', 'test')).toBe('2plop4');
+    expect(testDestructuring([1, 2, 3], { name: 'plop' }, 1, 2, 'plop', 'test')).toBe('2plop4');
   });
 
   it('Test testDestructuring() 2', () => {
-      expect(testDestructuring([1,2,3], {name: 'foo'}, 1, 2, 'plop')).toBe('2foo3');
+    expect(testDestructuring([1, 2, 3], { name: 'foo' }, 1, 2, 'plop')).toBe('2foo3');
   });
 
   it('Test testDestructuring() 3', () => {
-      expect(testDestructuring([1,2,3], undefined, 1, 2, 'plop')).toBe('2toto3');
+    expect(testDestructuring([1, 2, 3], undefined, 1, 2, 'plop')).toBe('2toto3');
+  });
+
+  it('Test updateUserInformation() 1', () => {
+    expect(updateUserInformation(userList, 236, { name: 'John' })).toEqual({
+      234: {
+        id: 234,
+        name: 'Paul',
+        city: 'London'
+      },
+      235: {
+        id: 235,
+        name: 'Pierre',
+        city: 'Paris'
+      },
+      236: {
+        id: 236,
+        name: 'John',
+        city: 'Lyon'
+      }
+    });
+  });
+
+  it('Test updateUserInformation() 2', () => {
+    expect(updateUserInformation(userList, 234, { name: 'John', city: 'Paris' })).toEqual({
+      234: {
+        id: 234,
+        name: 'John',
+        city: 'Paris'
+      },
+      235: {
+        id: 235,
+        name: 'Pierre',
+        city: 'Paris'
+      },
+      236: {
+        id: 236,
+        name: 'Jack',
+        city: 'Lyon'
+      }
+    });
   });
 
 });
